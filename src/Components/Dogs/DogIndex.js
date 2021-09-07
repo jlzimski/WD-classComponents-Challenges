b05efe9192fa0e39a5d9ba3a232388b42f237c6b
jlzimski <jlzimski@gmail.com>
2021-09-06 21:31:25 -0400

    finished playing Fetch

---
+++ src/Components/Dogs/DogIndex.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class DogIndex extends Component {
+    constructor() {
+        super();
+        this.url = "https://dog.ceo/api/breeds/image/random";
+        this.state = {
+            image: "",
+        };
+    }
+
+    componentDidMount() {
+        this.fetchFido();
+    }
+
+    fetchFido = () => {
+        fetch(this.url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data.status === "success") {
+                    this.setState({ image: data.message });
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+
+
+
+    render() {
+        return (
+            <div className="dogIndexMain">
+                <button className="fetchFido" onClick={ () => this.fetchFido()} >Fetch Fido!</button>
+                <hr/>
+                <div className="shotsOfSpot">
+                    <img className="shotsOfSpot" src={this.state.image} alt="doggo" />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DogIndex;
